46639c422e0e144557f031e81e6698045bf4cf13
Mitul Mungase <120619177+Mitul30M@users.noreply.github.com>
2023-08-27 09:43:30 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/1PbKgMLC5E25ov72/scene.splinecode');
